387455728576f716624dae9027cdf0ebb38a9c7e
Ferdian Noprianto <62186415+Ferdian-Noprianto@users.noreply.github.com>
2021-04-02 03:03:48 +0900

    Create latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,19 @@
+<?php
+
+$harga = 100000;
+$jumlah = 10;
+echo "<table border = '1'>
+        <tr>
+            <td><b>Jumlah Barang</b></td>
+            <td><b>Harga Barang</b></td>
+        </tr>";
+while ($jumlah <= 100) {
+    echo "<tr><td>";
+    echo $jumlah;
+    echo "</td><td>";
+    echo "Rp. " . $harga * $jumlah;
+    echo "</td><tr>";
+    $jumlah += 10;
+}
+
+echo "</table>";
